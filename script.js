aebf4190fb4e1352196ca46440001653e0da444d
Noah Huck <noahrhucku@gmail.com>
2023-12-24 19:54:21 -0800

    add default time on page load

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+/**
+ * Fixes the display of an input to maintain a valid time format
+ * @param {HTMLElement} input - The HTML input calling the function
+ */
 function fixDisplay(input) {
-  input.value = /.*[^\d].*/.test(input.value) ? "00" : 
-                input.id !== "display-hours" && input.value > 59 ? "59" : 
-                input.value.padStart(2, "0");
+  input.value = /.*[^\d].*/.test(input.value)
+    ? "00"
+    : input.id !== "display-hours" && input.value > 59
+    ? "59"
+    : input.value.padStart(2, "0");
 }
+
+function main() {
+  document
+    .querySelectorAll(".display")
+    .forEach(
+      (input) => (input.value = input.id == "display-minutes" ? "05" : "00")
+    );
+}
+
+main();
