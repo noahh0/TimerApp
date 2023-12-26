e8d0064a0f35ca06f35f369e339378b654e1f70c
Noah Huck <noahrhucku@gmail.com>
2023-12-25 22:24:42 -0800

    add countdown and button functionality

--- script.js
+++ script.js
@@ -1,21 +1,81 @@
+let currentTimer;
+let displays = document.querySelectorAll(".display");
+let startButton = document.querySelector("#start");
+let pauseButton = document.querySelector("#pause");
+let clearButton = document.querySelector("#clear");
+
 /**
  * Fixes the display of an input to maintain a valid time format
  * @param {HTMLElement} input - The HTML input calling the function
  */
-function fixDisplay(input) {
-  input.value = /.*[^\d].*/.test(input.value)
+function fixDisplay(display) {
+  display.value = /.*[^\d].*/.test(display.value)
     ? "00"
-    : input.id !== "display-hours" && input.value > 59
+    : display.id !== "display-hours" && display.value > 59
     ? "59"
-    : input.value.padStart(2, "0");
+    : display.value.padStart(2, "0");
+}
+
+/**
+ *
+ */
+function decrementTimer() {
+  if (displays[2].value === "00") {
+    if (displays[1].value === "00") {
+      if (displays[0].value === "00") {
+        timerAlert();
+      } else {
+        displays[0].value = String(displays[0].value - 1).padStart(2, "0");
+        displays[1].value = "59";
+        displays[2].value = "59";
+      }
+    } else {
+      displays[1].value = String(displays[1].value - 1).padStart(2, "0");
+      displays[2].value = "59";
+    }
+  } else {
+    displays[2].value = String(displays[2].value - 1).padStart(2, "0");
+  }
+}
+
+/**
+ *
+ */
+function pauseTimer() {
+  clearInterval(currentTimer);
+  displays.forEach((display) => display.removeAttribute("readonly"));
+  startButton.removeAttribute("disabled");
+}
+
+/**
+ *
+ */
+function timerAlert() {
+  pauseTimer();
+  // play alert sound
+}
+
+/**
+ *
+ */
+function startTimer() {
+  displays.forEach((display) => display.setAttribute("readonly", "readonly"));
+  startButton.setAttribute("disabled", "disabled");
+  currentTimer = setInterval(() => decrementTimer(), 1000);
+}
+
+/**
+ *
+ */
+function clearDisplay() {
+  pauseTimer();
+  displays.forEach(
+    (input) => (input.value = input.id == "display-minutes" ? "05" : "00")
+  );
 }
 
 function main() {
-  document
-    .querySelectorAll(".display")
-    .forEach(
-      (input) => (input.value = input.id == "display-minutes" ? "05" : "00")
-    );
+  clearDisplay();
 }
 
 main();
